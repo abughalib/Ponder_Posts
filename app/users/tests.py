71c74f9966d3c0d16c2af1e716d795a8902e11f1
Abu <abughalib64@gmail.com>
2022-07-26 18:42:49 +0530

    user creation test

--- app/users/tests.py
+++ app/users/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.contrib.auth.models import AnonymousUser, User
 
-# Create your tests here.
+from users.models import User
+
+
+class UserCreationTest(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create_user(
+            username='tester',
+            email='test_user@example.com',
+            password='top_secret_password'
+        )
+        self.assertEqual(user is not None, True)
